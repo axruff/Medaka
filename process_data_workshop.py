bfce6e9c481b5d74b3897b1565258a3df554f2b3
Alexey Ershov <39738572+axruff@users.noreply.github.com>
2024-01-15 18:19:39 +0100

    UPD: New alignment, same results

--- process_data_workshop.py
+++ process_data_workshop.py
@@ -3,7 +3,10 @@ import multiprocessing as mp
 from scipy.spatial.transform import Rotation as R
 import numpy as np
 import math
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+#from sympy import Point, Line
+
+from Geometry3D import *
 
 import time
 import os
@@ -120,7 +123,9 @@ def create_two_vector_sets(landmarks):
     right = landmarks[4]
     end = landmarks[5]
 
+    # Old routine
     center = np.floor((up + down) / 2.0)
+    print('Old center: ', center)
 
     vec_tip = tip - center
     vec_up = up - center
@@ -142,11 +147,88 @@ def create_two_vector_sets(landmarks):
     
     return norm_vectors, land_vectors
 
+def create_two_vector_sets_v2(landmarks):
+    
+    tip = landmarks[0]
+    up = landmarks[1] 
+    down = landmarks[2]  
+    #left = landmarks[3] # Old
+    #right = landmarks[4] # Old
+    left = landmarks[4] # Old
+    right = landmarks[3] # Old
+    end = landmarks[5]
+
+    center = get_center(landmarks)
+    #center_std = np.asarray([np.std([p1[0], p2[0], p3[0]]), np.std([p1[1], p2[1], p3[1]]), np.std([p1[2], p2[2], p3[2]])])
+    
+    print('New center: ', center)
+
+    vec_tip = tip - center
+    vec_up = up - center
+    vec_down = down - center
+    vec_left = left - center
+    vec_right = right - center 
+    vec_end = end - center
+    
+    #print('Tip', vec_tip)
+    #print('Up', vec_up)
+    #print('Down', vec_down)
+    #print('Left', vec_left)
+    #print('Right', vec_right)
+    #print('End', vec_end)
+
+    land_vectors = [vec_tip.tolist(), vec_up.tolist(), vec_down.tolist(), vec_left.tolist(), vec_right.tolist(), vec_end.tolist()]
+    
+    #print('Landmarks:', land_vectors)
+
+    vec_tip0 = [0,0,vec_length(vec_tip)]
+    vec_up0 = [0,-vec_length(vec_up),0]
+    vec_down0 = [0,vec_length(vec_down),0]
+    vec_left0 = [-vec_length(vec_left),0,0]
+    vec_right0 = [vec_length(vec_right),0,0]
+    vec_end0 = [0,0,-vec_length(vec_end)]
+    
+    #print('Vec Tip', vec_tip0)
+    #print('Vec Up', vec_up0)
+    #print('Vec Down', vec_down0)
+    #print('Vec Left', vec_left0)
+    #print('Vec Right', vec_right0)
+    #print('Vec End', vec_end0)
+
+    norm_vectors = [vec_tip0, vec_up0, vec_down0, vec_left0, vec_right0, vec_end0]
+    
+    #print('Norms:', norm_vectors)
+    
+    return norm_vectors, land_vectors
+
 def get_center(landmarks):
     
+    # OLD
+    #up = landmarks[1]
+    #down = landmarks[2]
+    #center = np.floor((up + down) / 2.0)
+    
+    tip = landmarks[0]
     up = landmarks[1]
     down = landmarks[2]
-    center = np.floor((up + down) / 2.0)
+    left = landmarks[3]
+    right = landmarks[4]
+    end = landmarks[5]
+    
+    set_eps(1e-0)
+
+    line_tip_end = Segment(Point(tip[0], tip[1], tip[2]), Point(end[0], end[1], end[2]))
+    line_up_down = Segment(Point(up[0], up[1], up[2]), Point(down[0], down[1], down[2]))
+
+    p1 = intersection(line_tip_end, line_up_down)
+
+    line_left_right = Segment(Point(left[0], left[1], left[2]), Point(right[0], right[1], right[2]))
+
+    p2 = intersection(line_tip_end, line_left_right)
+
+    p3 = intersection(line_up_down, line_left_right)
+
+    center = np.asarray([np.mean([p1[0], p2[0], p3[0]]), np.mean([p1[1], p2[1], p3[1]]), np.mean([p1[2], p2[2], p3[2]])])
     
     return center
 
@@ -175,14 +257,26 @@ def process_dataset(d):
     datasets_202012 = f.readlines()
     f.close()
     
+    f = open(path_datasets_list / 'datasets_201811.txt', 'r')
+    datasets_201811 = f.readlines()
+    f.close()
+    
+    if ind in [int(x.split('_')[1]) for x in datasets_201811]:
+        data_path = "2018_11"
+        server_path = "/mnt/LSDF/projects/code-vita/Medaka/" 
+        
     if ind in [int(x.split('_')[1]) for x in datasets_201905]:
         data_path = "201905_beamtime_medaka_stained"
+        server_path = "/mnt/HD-LSDF/Medaka/" 
         
     if ind in [int(x.split('_')[1]) for x in datasets_201912]:
         data_path = "201912_beamtime_medaka"
+        server_path = "/mnt/HD-LSDF/Medaka/"
         
     if ind in [int(x.split('_')[1]) for x in datasets_202012]:
         data_path = "202012_beamtime_medaka"
+        server_path = "/mnt/HD-LSDF/Medaka/"
+        
         
     
     # OLD: Depricated
@@ -195,7 +289,8 @@ def process_dataset(d):
     #data_path = "201912_beamtime_medaka"
     
     
-    path = Path("/mnt/HD-LSDF/Medaka/" + data_path + "/" + dataset +"/scaled_0.5_8bit_slices.tif")
+    #path = Path("/mnt/HD-LSDF/Medaka/" + data_path + "/" + dataset +"/scaled_0.5_8bit_slices.tif")
+    path = Path(server_path + data_path + "/" + dataset +"/scaled_0.5_8bit_slices.tif")
     
     # Read data
     print(f'Reading image: {dataset}')
@@ -216,17 +311,28 @@ def process_dataset(d):
     #print('Data size (x,y,z):', w,h,d)
     #print('Data center (x,y,z):', w/2,h/2,d/2)
 
-    vec_norm, vec_land = create_two_vector_sets(land)
-    
+    vec_norm, vec_land = create_two_vector_sets(land) 
     res_rot = R.align_vectors(vec_land, vec_norm)
 
     # Find rotations
     rotation_degrees = res_rot[0].as_euler('zyx', degrees=True)
-    print('Calculated rotations:', rotation_degrees)
+    print(f'{dataset}: Calculated rotations (old):', rotation_degrees)
     degrees = rotation_degrees
+    
+    vec_norm, vec_land = create_two_vector_sets_v2(land) 
+    res_rot = R.align_vectors(vec_land, vec_norm) 
+
+    # Find rotations
+    rotation_degrees = res_rot[0].as_euler('zyx', degrees=True)
+    print(f'{dataset}: Calculated rotations (NEW):', rotation_degrees)
+    degrees = rotation_degrees
+    
+    #return
+    
 
     img_translation = Affine(scales=[1.0, 1.0, 1.0], degrees=[0,0,0], translation=[d/2-c[2],h/2-c[1],w/2-c[0]], center='image')
     img_rotation = Affine(scales=[1.0, 1.0, 1.0], degrees=degrees, translation=[0,0,0], center='image')
+    #img_rotation = Affine(scales=[1.0, 1.0, 1.0], degrees=[rotation_degrees[0], rotation_degrees[2], -rotation_degrees[1]], translation=[0,0,0], center='image')
 
     print('Transforming...')
     
@@ -299,9 +405,9 @@ def process_dataset(d):
     #fig.savefig(path_output / f'{dataset}_test.png')
 
 path_datasets_list = Path("/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/")
-path_landmarks = Path("/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/landmarks_mitk/")
-path_output = Path("/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/data/")
-path_preview = Path("/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/preview/")
+path_landmarks = Path("/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/landmarks_part09/")
+path_output = Path("/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/data_part09/")
+path_preview = Path("/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/preview_part09/")
     
 def main(): 
     
@@ -309,10 +415,7 @@ def main():
 
     
     print('Hi')
-    
-    # Manual
-    #dataset_list = ['Medaka_1064_4-1', 'Medaka_1071_8-2', 'Medaka_1075_13-2']
-    
+     
     #df = pd.read_excel(open(path_datasets_list / 'evaluation_segmentation.xlsx', 'rb'), sheet_name='selected')
     #df['dataset'].tolist()
     
@@ -320,21 +423,24 @@ def main():
     f = open(path_datasets_list / 'selected_datasets.txt', 'r')
     lines = f.readlines()
     print('Selected fish:', lines)
+    print('In total:', len(lines))
     f.close()
     
     aligned_datasets = [x.split('.')[0] for x in os.listdir(path_landmarks) if '.mps' in x]
     processed_datasets = [x.split('.')[0] for x in os.listdir(path_output) if '.tif' in x]
     
     print('Aligned', aligned_datasets)
+    print('In total', len(aligned_datasets))
     
+    # Maual
+    #dataset_list = ['Medaka_423_11-1']
     dataset_list = [x.strip() for x in lines if (x.split('_')[1] in aligned_datasets) and (x.split('_')[1] not in processed_datasets) ]
     
     print('Datasets to process:', dataset_list)
+    print('In total:', len(dataset_list))
     
-    
-   
                              
-    pool = mp.Pool(10)
+    pool = mp.Pool(15)
     res = pool.map(process_dataset, dataset_list)
     
     elapsed = (time.time() - start) / 60
