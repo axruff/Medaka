92616b14601603bc371a20099013702c638506f3
Alexey Ershov <39738572+axruff@users.noreply.github.com>
2024-05-14 15:49:45 +0200

    UPD: Visualize single landmark

--- landmarks_visualize_by_landmarks.py
+++ landmarks_visualize_by_landmarks.py
@@ -344,8 +344,102 @@ def make_landmarks_comparison_figure_centered(im, file_name, land1, land2, landm
     #plt.show()
     plt.savefig(f'{file_name}.png')
     
+    
+def make_single_landmark_figure_centered(im, land1, landmark_name, name1, color1 = 'red'):
 
-def visualize_result(index):
+    marker_size = 5
+    marker_alpha = 0.7
+    line_width = 0.5
+
+    x = math.floor(land1[0])
+    y = math.floor(land1[1])
+    z = math.floor(land1[2])
+    
+    zero_land1 = ' (ZERO)' if x == 0 and y == 0 and z == 0 else ''
+
+    fig = plt.figure(figsize= (14,3), layout="tight")
+
+    gs = GridSpec(1, 3, figure=fig)
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax3 = fig.add_subplot(gs[0, 1])
+    ax5 = fig.add_subplot(gs[0, 2])
+
+    for i, ax in enumerate(fig.axes):
+        ax.set_xticks([])
+        ax.set_yticks([])  
+
+    fig.suptitle(f"{landmark_name}", color='white')
+    #fig.suptitle(f"{landmark_name}    [{name1} and {name2}]    Diff = {diff} px", color='white')
+
+    fig.patch.set_facecolor('black')
+
+    #-------------------------
+    # Front 1
+    #-------------------------
+    im_cr = center_crop_with_padding(im[z],x,y)
+    cx,cy = int(im_cr.shape[1]/2), int(im_cr.shape[0]/2)
+    ax5.imshow(im_cr, cmap='gray')
+    # Markers
+    ax5.plot(cx,cy,'ro', markersize=marker_size, alpha=marker_alpha)
+    
+    #cx2,cy2 = cx +(x2-x),cy + (y2-y)
+    #if not (cx2 > im_cr.shape[1] or cx2 < 0 or cy2 > im_cr.shape[0] or cy2 < 0):
+        #print('Fuck, out')
+    #    ax5.plot(cx2,cy2,'bo', markersize=marker_size, alpha=marker_alpha)
+
+    # Lines
+    #ax5.plot([0, im.shape[2]-1], [y, y], 'r', linestyle = 'dashed', linewidth=line_width)
+    #ax5.plot([x, x], [0, im.shape[1]-1], 'r', linestyle = 'dashed', linewidth=line_width)
+
+    ax5.text(0.01, 0.95, name1 + zero_land1, horizontalalignment='left',
+         verticalalignment='center', transform=ax5.transAxes, bbox=dict(facecolor='black', alpha=0.5), color=color1, fontsize=12)
+
+
+    #-------------------------
+    # Top 1
+    #-------------------------
+    im_cr = center_crop_with_padding(im[:, y, :].T,z,x)
+    cx,cy = int(im_cr.shape[1]/2), int(im_cr.shape[0]/2)
+    ax1.imshow(im_cr, cmap='gray')
+    ax1.plot(cx,cy,'ro', markersize=marker_size, alpha=marker_alpha)
+    #ax1.plot(z2,x2,'bo', markersize=marker_size, alpha=marker_alpha) # Old
+    #cx2,cy2 = cx +(z2-z),cy + (x2-x)
+    #if not (cx2 > im_cr.shape[1] or cx2 < 0 or cy2 > im_cr.shape[0] or cy2 < 0):
+    #    ax1.plot(cx2,cy2,'bo', markersize=marker_size, alpha=marker_alpha)
+    
+    # Lines
+    #ax1.plot([0, im.shape[0]-1], [x, x], 'r', linestyle = 'dashed', linewidth=line_width)
+    #ax1.plot([z, z], [0, im.shape[2]-1], 'r', linestyle = 'dashed', linewidth=line_width)
+
+    ax1.text(0.01, 0.95, name1+zero_land1, horizontalalignment='left',
+         verticalalignment='center', transform=ax1.transAxes, bbox=dict(facecolor='black', alpha=0.5), color=color1, fontsize=12)
+   
+    #-------------------------
+    # Side 1
+    #-------------------------
+    im_cr = center_crop_with_padding(im[:, :, x].T,z,y)
+    cx,cy = int(im_cr.shape[1]/2), int(im_cr.shape[0]/2)
+    ax3.imshow(im_cr, cmap='gray')
+    ax3.plot(cx,cy,'ro', markersize=marker_size, alpha=marker_alpha)
+    #cx2,cy2 = cx + (z2-z),cy + (y2-y)
+    #if not (cx2 > im_cr.shape[1] or cx2 < 0 or cy2 > im_cr.shape[0] or cy2 < 0):
+    #    ax3.plot(cx2,cy2,'bo', markersize=marker_size, alpha=marker_alpha)
+
+    # Lines
+    #ax3.plot([0, im.shape[0]-1], [y, y], 'r', linestyle = 'dashed', linewidth=line_width)
+    #ax3.plot([z, z], [0, im.shape[1]-1], 'r', linestyle = 'dashed', linewidth=line_width)
+
+    ax3.text(0.01, 0.95, name1+zero_land1, horizontalalignment='left',
+         verticalalignment='center', transform=ax3.transAxes, bbox=dict(facecolor='black', alpha=0.5), color=color1, fontsize=12)
+
+   
+
+    #plt.show()
+    #plt.savefig(f'res_centered_eval.png')
+    plt.savefig(f'{file_name}.png')
+    
+
+def visualize_pair_result(index):
 
     pos = index[0]
     i = index[1]
@@ -382,11 +476,43 @@ def visualize_result(index):
                                      [x,y,z], [x2,y2,z2], f'{pointset}: {landmark}', 
                                      participants_names[name1], participants_names[name2])
     
+def visualize_single_result(index):
+
+    pos = index[0]
+    i = index[1]
+    
+    sample = top_df.loc[i]['sample']
+    pointset = top_df.loc[i]['point_set']
+    name1 = top_df.loc[i]['name']
+    #name2 = top_df.loc[i]['name2']
+    landmark = top_df.loc[i]['landmark']
+
+
+    x,y,z = get_landmark_coords_from_table(res_df, sample, pointset, landmark, name1)
+    #print([x,y,z])
+
+    #x2,y2,z2 = get_landmark_coords_from_table(res_df, sample, pointset, landmark, name2)
+    #print([x,y,z])
+
+    # Read sample
+
+    #print(f'Processing: {sample}')
+    sitk_image = sitk.ReadImage(volumes_path / f'{sample}.tif')
+    im = sitk.GetArrayViewFromImage(sitk_image)
+    #print('OK')
+    
+    landmark_index = get_landmark_index(all_landmarks, pointset, landmark)
+
+    make_single_landmark_figure_centered(im, path_out / f'{str(pos).zfill(4)}_{sample}_{pointset}_{str(landmark_index).zfill(2)}_{landmark}_{name1}', 
+                                     [x,y,z], f'{pointset}: {landmark}', 
+                                     participants_names[name1])
+    
     
     
 if __name__ == "__main__":
     
-    VERSION = 'diff_v7_v8'
+    #VERSION = 'diff_v7_v8'
+    VERSION = 'v9_eyes'
     
     path_eval = Path(f"/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/evaluation/{VERSION}/")
     path_output = Path(f"/mnt/LSDF/tomo/ershov/medaka/workshop_landmarks/evaluation_vis/by_landmark_centered_{VERSION}/")
@@ -402,13 +528,15 @@ if __name__ == "__main__":
     
     res_df = pd.read_excel(path_eval / 'results_landmarks_v8.xlsx')
     
-    for ps in landmarks_pointset_names:
+    #for ps in landmarks_pointset_names:
     #for ps in ['pointset1_vert', 'pointset2_fins']:
+    for ps in ['pointset5_eyes']:
     
         print('Processing pointset: ', ps)
 
 
-        eval_df = pd.read_excel(path_eval / f'eval_{ps}_{VERSION}.xlsx')
+        #eval_df = pd.read_excel(path_eval / f'eval_{ps}_{VERSION}.xlsx')
+        eval_df_eyes = pd.read_excel(path_eval / f'eval_pointset5_eyes_v9_ast_without_thomas.xlsx')
         
         landmarks_list = list(eval_df["landmark"].unique())
         
@@ -439,7 +567,8 @@ if __name__ == "__main__":
             #print(list(zip(indexes, proc_list)))
 
             pool = mp.Pool(30)
-            res = pool.map(visualize_result, list(zip(indexes, proc_list)))
+            #res = pool.map(visualize_pair_result, list(zip(indexes, proc_list))) 
+            res = pool.map(visualize_single_result, list(zip(indexes, proc_list))) 
 
         
     elapsed = (time.time() - start) / 60  
